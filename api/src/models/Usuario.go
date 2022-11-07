c3a4ea1debad6ce56c011c408d0c7f28e0e7e6f0
Ernane Sabino <esabino@stefanini.com>
2022-11-07 07:44:48 -0300

    DEVBOOK :: Validação dos campos do usuário

--- api/src/models/Usuario.go
+++ api/src/models/Usuario.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario representa um usuário da rede social
 type Usuario struct {
@@ -10,4 +14,40 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar o usuário recebido
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O nick é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O e-mail é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("A senha é obrigatória e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (usario *Usuario) formatar() {
+	usario.Nome = strings.TrimSpace(usario.Nome)
+	usario.Nick = strings.TrimSpace(usario.Nick)
+	usario.Email = strings.TrimSpace(usario.Email)
 }
