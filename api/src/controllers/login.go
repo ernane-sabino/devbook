51d545d61bd77bbe5aa3c3408f785bcdaa7b3a9d
Ernane Sabino <esabino@stefanini.com>
2022-11-11 08:45:41 -0300

    DEVBOOK :: Rota e validação de login

---
+++ api/src/controllers/login.go
@@ -0,0 +1,48 @@
+package controllers
+
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"api/src/respostas"
+	"api/src/seguranca"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+// Login é responsável por autenticar um usuário na API
+func Login(w http.ResponseWriter, r *http.Request) {
+	corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario models.Usuario
+	if erro := json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := database.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositories.NovoRepositorioUsuarios(db)
+	usuarioSalvoNoBanco, erro := repositorio.BuscarPorEmail(usuario.Email)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if erro = seguranca.VerificarSenha(usuarioSalvoNoBanco.Senha, usuario.Senha); erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	w.Write([]byte("Você está logado!"))
+}
--- api/src/repositories/usuarios.go
+++ api/src/repositories/usuarios.go
@@ -141,3 +141,22 @@ func (repositorio Usuarios) Deletar(ID uint64) error {
 
 	return nil
 }
+
+// BuscarPorEmail busca um usuário por e-mail e retorna seu id e senha com hash
+func (repositorio Usuarios) BuscarPorEmail(email string) (models.Usuario, error) {
+	linha, erro := repositorio.db.Query("select id, senha from usuarios where email = ?", email)
+	if erro != nil {
+		return models.Usuario{}, erro
+	}
+	defer linha.Close()
+
+	var usuario models.Usuario
+
+	if linha.Next() {
+		if erro = linha.Scan(&usuario.ID, &usuario.Senha); erro != nil {
+			return models.Usuario{}, erro
+		}
+	}
+
+	return usuario, nil
+}
